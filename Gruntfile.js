deece32930190e33ad85fe44bcdc18eb16274fc8
JacobWay <soarinblue@gmail.com>
2016-11-18 22:32:11 +0800

    jslint sass concat functions

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,34 @@
-//
-// http://24ways.org/2013/grunt-is-not-weird-and-hard/
-//
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'pre_css',
+          src: ['*.scss'],
+          dest: 'css',
+          ext: '.css'
+        }],
+      },
+    },
+    jshint: {
+      files: ["scripts/*.js"],
+    },
+    concat: {
+      js: {
+        src: ["scripts/*.js"],
+        dest: "dist/<%= pkg.name %>.js"
+      },
+      options: {
+        // Task-level options may go here, overriding task defaults.
+      },
+    },
     "connect": {
       keepalive: {
         options: {
           port: 8989,
-          host: "localhost",
+          host: "*",
           livereload: true,
           open: "http://localhost:8989/html/main.html"
         }
@@ -16,11 +36,13 @@ module.exports = function(grunt) {
     },
     "watch": {
       "build": {
-        files: ["scripts/main.js", "html/*.html"],
+        files: ["scripts/main.js", "html/*.html", "css/*.css"],
         options: {
           livereload: true
         }
-      }
+      },
+      files: ["scripts/*.js", "Gruntfile.js", "pre_css/*.scss"],
+      tasks:  ["jshint", "sass", "concat"]
     },
   });
 
@@ -32,12 +54,17 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-concurrent");
   grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  //grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-sass");
 
 
   //
   // Register tasks
   //
-  grunt.registerTask("serve", ["connect", "watch:build"]);
+  grunt.registerTask("default", ["connect", "watch"]);
+  grunt.registerTask("build", ["jshint", "sass", "concat"]);
 };
 
 
--- readme.md
+++ readme.md
@@ -1,9 +1,43 @@
 install require js
 install grunt js
 install grunt-contrib-watch
+install grunt-contrib-connect
 install grunt-contrib-sass
 install sass: gem install sass
 install grunt-express. for live reloadin
+
+Function Lists:
+Live reloading web server.
+JS lint
+Sass preprocess compile.
+File concat.
+
+Undone Lists:
+Uglify files.
+Test app.
+Performance test.
+
+
+Config an live reloading server.
+install grunt-contrib-watch, grunt-contrib-connect.
+In connect, watch directives, there is a parameter livereload, configing it to true.
+connect: {
+  options: {
+    livereload: true,
+  }
+}
+watch: {
+  options: {
+    livereload: true,
+  }
+}
+caveat:
+Don't config keepalive parameter in connect directive. It will block watch task.
+keepalive: true // Don't
+
+
+JS lint
+Install grunt-contrib-jshint
 
 play with require js
 
